9a1dba9b31bbb6e13a52b163a40f46437a125c05
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2023-02-18 19:19:49 +0100

    feat: create blueprint structure
    Created blueprints for auth and route files linking both to the flask app

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from auth import auth_blueprint
+from routes import route_blueprint
 from os import urandom
 
 
 app = Flask(__name__)
+app.register_blueprint(auth_blueprint)
+app.register_blueprint(route_blueprint)
 app.app_context().push()
 secret_key = urandom(32)
 app.config['SECRET_KEY'] = secret_key
@@ -11,13 +15,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///apinote.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app)
-
-
-@app.route('/')
-def hello_world():  # put application's code here\
-    """Welcomes client and redirects to API documentation"""
-    return jsonify({'message': 'Welcome to apinote'})
-
 
 if __name__ == '__main__':
     app.run(DEBUG=True)
