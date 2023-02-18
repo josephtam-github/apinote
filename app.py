2ebf82c7865336c21998e195b1f5aa1c0b567f96
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2023-02-18 21:49:16 +0100

    fix: Reorder code to prevent circular import
    Circular import of db from auth.py and models.py due to blueprint import

--- app.py
+++ app.py
@@ -1,23 +1,24 @@
 from flask import Flask
 from flask_marshmallow import Marshmallow
-from flask_restful import Api, Resource
+from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
-from auth import auth_blueprint
-from routes import route_blueprint
 from os import urandom
 
 
 app = Flask(__name__)
-app.register_blueprint(auth_blueprint)
-app.register_blueprint(route_blueprint)
 app.app_context().push()
 app.config['SECRET_KEY'] = urandom(24)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///apinote.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
-db = SQLAlchemy(app)
 api = Api(app)
 ma = Marshmallow(app)
+db = SQLAlchemy(app)
+
+from auth import auth_blueprint
+from routes import route_blueprint
+app.register_blueprint(auth_blueprint)
+app.register_blueprint(route_blueprint)
 
 if __name__ == '__main__':
     app.run()
