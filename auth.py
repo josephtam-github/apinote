80660a42a58959b7b17c211aa289adba3982a4a5
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2023-02-18 18:55:25 +0100

    feat: create login route
    Checks for correct login details then generates a random token for the user to access the note api

--- auth.py
+++ auth.py
@@ -40,3 +40,20 @@ def signup_user():
 
     return jsonify({'message': 'registered successfully'})
 
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_user():
+    auth = request.authorization
+
+    if not auth or not auth.username or not auth.password:
+        return make_response('verification failed', 401, {'WWW.Authentication': 'Basic realm: "login required"'})
+
+    user = Users.query.filter_by(name=auth.username).first()
+
+    if check_password_hash(user.password, auth.password):
+        token = jwt.encode(
+            {'public_id': user.public_id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)},
+            app.config['SECRET_KEY'])
+        return jsonify({'token': token.decode('UTF-8')})
+
+    return make_response('verification failed', 401, {'WWW.Authentication': 'Basic realm: "login required"'})
