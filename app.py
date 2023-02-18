93286ecdba504bc61923b256d7613e437b09b5ff
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2023-02-18 20:43:07 +0100

    feat: import flask_restful and flask_marshmallow for API

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask_marshmallow import Marshmallow
+from flask_restful import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
 from auth import auth_blueprint
 from routes import route_blueprint
@@ -9,12 +11,13 @@ app = Flask(__name__)
 app.register_blueprint(auth_blueprint)
 app.register_blueprint(route_blueprint)
 app.app_context().push()
-secret_key = urandom(32)
-app.config['SECRET_KEY'] = secret_key
+app.config['SECRET_KEY'] = urandom(24)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///apinote.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app)
+api = Api(app)
+ma = Marshmallow(app)
 
 if __name__ == '__main__':
-    app.run(DEBUG=True)
+    app.run()
