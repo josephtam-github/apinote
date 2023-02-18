83db23735040a62f341ff9504e0cde1a8eb6bcb4
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2023-02-18 16:39:33 +0100

    feat: configure and import SQLAchemy

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 from flask import Flask
+from flask import Flask, request, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
+from os import urandom
+
+
 app = Flask(__name__)
+app.app_context().push()
+app.config['SECRET_KEY'] = urandom(32)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///apinote.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return jsonify({'message': 'apinote'})
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(DEBUG=True)
