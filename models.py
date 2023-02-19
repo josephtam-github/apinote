e90b8a490eba8384b263b8a4a18f39ab8fe06c77
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2023-02-19 19:38:31 +0100

    refactor: change users model relationship 'notes' to 'notes_by'
    This avoids mix up as a model is already named notes

--- models.py
+++ models.py
@@ -9,7 +9,7 @@ class Users(db.Model):
     name = db.Column(db.String(50))
     password = db.Column(db.String(50))
     admin = db.Column(db.Boolean)
-    notes = db.relationship('Notes', back_populates='created_by', lazy='dynamic')
+    notes_by = db.relationship('Notes', back_populates='created_by', lazy='dynamic')
 
     def __repr__(self):
         return f'User {self.username}'
@@ -22,7 +22,8 @@ class Notes(db.Model):
     content = db.Column(db.String(2048), nullable=False)
     # A full type-written page usually contains 2kb of character hence 2048 char limit
     created_on = db.Column(db.DateTime, default=datetime.now())
-    created_by = db.relationship('Users', back_populates='notes')
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), unique=False, nullable=False)
+    created_by = db.relationship('Users', back_populates='notes_by')
 
     def __repr__(self):
         return f'User {self.username}'
